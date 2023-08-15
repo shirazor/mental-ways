dbd0c2ca0863f2e08c680032ceb0276071e4fccd
itay <itay.jacobson@gmail.com>
2023-08-15 16:18:07 +0300

    working on chat mock

--- MentalWays/ChatScreen.js
+++ MentalWays/ChatScreen.js
@@ -4,25 +4,118 @@ import { GiftedChat } from 'react-native-gifted-chat';
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
+  const conversationFlow = [
+    {
+      question: "What's your favorite color?",
+      answers: ["Red", "Blue", "Green"],
+    },
+    {
+      question: "How old are you?",
+      answers: ["Under 18", "18-30", "31+"],
+    },
+    // Add more questions here
+  ];
+
+  const conversationMock = {
+    _id: 1,
+    title: 'hello',
+    initialStep: stepOne
+  };
+
+  const stepOne = {
+    _id: 1,
+    question: "ask next questio about:",
+    answers: [
+        {
+            value: "amit michles",
+            nextStep: steptwo
+        },
+        {
+            value: "or harush",
+            nextStep: stepThree
+        }
+    ]
+  }
+
+  const steptwo = {
+    _id: 2,
+    question: "amit favorite food is",
+    answers: [
+        {
+            value: "hamburger",
+            nextStep: stepFour
+        },
+        {
+            value: "pizza",
+            nextStep: stepFour
+        },
+        {
+            value: "back to first question",
+            nextStep: stepOne
+        }
+    ]
+  }
+
+  const stepThree = {
+    _id: 3,
+    question: "ot harush lives in",
+    answers: [
+        {
+            value: "raanana",
+            nextStep: stepFour
+        },
+        {
+            value: "eilat",
+            nextStep: stepFour
+        },
+        {
+            value: "back to first question",
+            nextStep: stepOne
+        }
+    ]
+  }
+
+  const stepFour = {
+    
+  }
+
+
+  const botUser = { _id: 2, name: 'Chatbot' };
+  const [currentQuestionIndex, setIt] = useState(0);
 
   const onSend = newMessages => {
-    const options = ['Option 1', 'Option 2', 'Option 3'];
-    const quickReplies = options.map(option => ({
-      title: option,
-      value: option,
-    }));
-
-    const newMessage = {
+    const userMessage = newMessages[0].text;
+    const currentQuestion = conversationFlow[currentQuestionIndex];
+    
+    const userResponseMessage = {
       _id: Math.random().toString(),
-      text: newMessages[0].text,
+      text: userMessage,
       createdAt: new Date(),
       user: newMessages[0].user,
-      quickReplies,
     };
 
     setMessages(prevMessages =>
-      GiftedChat.append(prevMessages, [newMessage])
+      GiftedChat.append(prevMessages, [userResponseMessage])
     );
+
+    if (currentQuestion) {
+      const nextQuestionMessage = {
+        _id: Math.random().toString(),
+        text: currentQuestion.question,
+        createdAt: new Date(),
+        user: botUser,
+        quickReplies: currentQuestion.answers.map(answer => ({
+          title: answer,
+          value: answer,
+        })),
+      };
+
+      setMessages(prevMessages =>
+        GiftedChat.append(prevMessages, [nextQuestionMessage])
+      );
+
+      setIt(x => x +1);
+    }
   };
 
   const renderQuickReplies = props => {
@@ -52,6 +145,7 @@ const ChatScreen = () => {
     };
 
     setMessages(prevMessages => GiftedChat.append(prevMessages, [message]));
+    onSend([{ text: value, user: { _id: 1 } }]);
   };
 
   return (
