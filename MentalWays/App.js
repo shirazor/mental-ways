e7c33d2f4aecd623ff817fe43c50f00b9ffc17e6
sagitaizi <sagisagi229000@gmail.com>
2023-08-16 10:02:23 +0300

    refactor(rtl): refactored to rtl

--- MentalWays/App.js
+++ MentalWays/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import CustomStack from './CustomStack';
+import CustomDrawer from './CustomDrawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { I18nManager } from 'react-native';
 
 const App = () => {
+  I18nManager.forceRTL(true);
   return (
     <NavigationContainer>
-      <CustomStack />
+      <CustomDrawer />
     </NavigationContainer>
   );
 };
