5a3e4f3f75ef2bd9931bc946f988ee689b842de9
sagitaizi <sagisagi229000@gmail.com>
2023-08-16 11:20:55 +0300

    feat(main-manage-screen): add main manage screen design

---
+++ MentalWays/ManageScreen/ChatListItem.js
@@ -0,0 +1,23 @@
+import { Text } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
+
+const ChatListItem = ({ item, onPress }) =>
+	<TouchableOpacity onPress={onPress} style={styles.item}>
+		<Text style={styles.text}>{item.title}</Text>
+	</TouchableOpacity>;
+
+const styles = StyleSheet.create({
+	item: {
+		backgroundColor: '#d9d9d9',
+		justifyContent: 'center',
+		padding: 10,
+		height: 80,
+		marginTop: 15
+	},
+	text: {
+		fontSize: 20,
+		marginLeft: 20
+	}
+});
+
+export default ChatListItem;
