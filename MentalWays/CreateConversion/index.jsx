4c74780bf47f054d04da6ce47259897a1c3cb1fb
itay <itay.jacobson@gmail.com>
2023-08-17 16:24:40 +0300

    fixed bug

--- MentalWays/CreateConversion/index.jsx
+++ MentalWays/CreateConversion/index.jsx
@@ -2,10 +2,6 @@ import NewConversion from "./NewConversation";
 import {NewConversationProvider} from './newConversationContext'
 
 const CreateConversion = () => {
-        id: 1, question: '', answers: [
-            { value: '', nextStep: {} },
-            { value: '', nextStep: {} }]
-
     return (
         <NewConversationProvider>
             <NewConversion />
